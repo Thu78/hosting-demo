d7e1bbf64cf2a533e43cd177db24b8762629d377
Jeffrey Liu <jltexas@utexas.edu>
2023-02-26 22:13:31 -0600

    Added AWS Preliminary

--- index.js
+++ index.js
@@ -1,38 +1,15 @@
 const express = require("express");
+const sls = require("serverless-http");
+
 // Loads environment variables
 require("dotenv").config();
 // Heroku will automatically have its own port
-const port = process.env.PORT || 6000;
 const app = express();
 app.use(express.json());
-
-// Connect firebase App: can leave commented out, loads environment variables
-const admin = require("firebase-admin");
-const adminConfig = require("./cred.json");
-admin.initializeApp({
-  credential: admin.credential.cert(adminConfig),
-});
-const database = admin.firestore();
-// Create an API Route: should be available on -> localhost:5000/user
-app.get("/user", async (req, res) => {
-  const user = await database.collection("user").doc("jonathan").get();
-  const data = user.data();
-  res.json({ msg: "Testing, sending hello", user: data });
-});
 
 // Create an API Route: should be available on -> localhost:5000/hello
 app.get("/hello", (req, res) => {
   res.json({ msg: "Testing, sending hellos" });
 });
 
-// Serve Frontend
-const path = require("path");
-app.use("/", express.static(path.resolve(__dirname, "my-app/build")));
-// Make sure this is last or else api will call not be able to call /hello
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "my-app/build", "index.html"));
-});
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+module.exports.server = sls(app);
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState,useEffect} from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
 
-// API PREFIX URL: Replace <YOUR_HEROKU_APP> with the actual heroku app
-const URL = process.env.NODE_ENV === "development" ? "http://localhost:5000/": window.location.href;
+// API PREFIX URL: Replace <AWS_URL> with the actual AWS app
+const URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000/"
+    : "<AWS_URL>";
 
 function App() {
-  const [text,setText] = useState("<DEFAULT TEXT>")
+  const [text, setText] = useState("<DEFAULT TEXT>");
   // Show you can get stuff from the server
-  useEffect(()=>{
-     fetch(`${URL}hello`,{
-      method:"GET",
+  useEffect(() => {
+    fetch(`${URL}hello`, {
+      method: "GET",
       mode: "cors",
       headers: {
-        'Content-Type': "application/json"
+        "Content-Type": "application/json",
       },
-    }).then(data =>{
-      return data.json()
-    }).then(data => {
-      const {msg} = data
-      setText(msg)
     })
-  },[])
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        const { msg } = data;
+        setText(msg);
+      });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {text}
-        </p>
+        <p>{text}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
